6ae8f86596e0a86ae219eaea3999d8ca56e05519
Valerie <blooprintcreation@gmail.com>
2025-10-13 21:18:00 +0200

    Add files via upload

---
+++ mini-cv-linked-in-bio-oct25.js
@@ -0,0 +1,22 @@
+﻿document.querySelectorAll('.accordion-header').forEach(header => {
+  header.addEventListener('click', () => {
+     const content = header.nextElementSibling;
+     
+     header.classList.toggle('active');
+    
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null; // ferme le panneau
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px"; // ouvre le panneau
+    }
+  });
+});
+
+// --- Toggle dark/light ---
+const toggle = document.getElementById('theme-toggle');
+toggle.addEventListener('change', () => {
+  document.body.classList.toggle('dark', toggle.checked);
+  localStorage.setItem('theme', toggle.checked ? 'dark' : 'light');
+});
+
+ 
